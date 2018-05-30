f6579ddc0ddd180988aed2c30c1b37b7c0fd7e8b
chocolava <viva.gamma.liecharlie@student.untan.ac.id>
2018-05-30 16:02:54 +0700

    belajar MVC

---
+++ mvc/model/Mahasiswa.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace model;
+
+use app\Model;
+
+
+/**
+* 
+*/
+class Mahasiswa extends Model
+{
+	public function ambilSemua()
+	{
+		$pesan = "Berhasil";
+		return $pesan;
+	}
+	
+}
+
